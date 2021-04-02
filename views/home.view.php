369e28fd257b5b5a8b3456f8491b881bf598687b
erik.masny <erik.masny@gmail.com>
2021-04-02 23:07:49 +0200

    Added 404 and home view

---
+++ views/home.view.php
@@ -0,0 +1,21 @@
+<?php include 'header.php' ?>
+
+<main>
+    <div class="container">
+
+        <table class="table">
+        
+            <tr>
+                <th>Študent</th>
+                <th>1</th>
+                <th>2</th>
+                <th>Počet účastí</th>
+                <th>Počet minút na prednáškach</th>
+            </tr>
+
+        </table>
+    
+    </div>
+</main>
+
+<?php include 'footer.php' ?>
