d575757cc1507f9515a9940e3ddc45e64a0f7e8c
erik.masny <erik.masny@gmail.com>
2021-04-02 23:28:46 +0200

    Configured webpack to compile sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,34 @@
 const path = require('path');
 
 module.exports = {
-    entry: './assets/src/js/main.js',
+    entry: [
+        './assets/src/js/main.js',
+        './assets/src/scss/main.scss'
+    ],
     output: {
-    filename: 'app.js',
+        filename: 'app.js',
         path: path.resolve(__dirname, 'assets/public/js'),
     },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [],
+            }, {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: { 
+                            outputPath: '../css/', 
+                            name: 'app.css'
+                        }
+                    },
+                    'sass-loader'
+                ]
+            }
+        ]
+    }
 };
