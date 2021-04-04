18b22c7a72fc85a8c7be8d8025f934e7876ba40a
erik.masny <erik.masny@gmail.com>
2021-04-04 18:21:35 +0200

    Added nav item for stats

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Zadanie 3 | Erik Masny</title>
+    <title>Zadanie 4 | Erik Masny</title>
 
     <!-- FONTS -->
 
@@ -26,6 +26,9 @@
         <ul class="navbar-nav">
             <li class="nav-item">
                 <a class="nav-link" href="<?= BASE_URL ?>">Domov</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?= BASE_URL ?>/stats">Stats</a>
             </li>
         </ul>
         
