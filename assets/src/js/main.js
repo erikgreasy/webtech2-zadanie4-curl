a6b8e714b563517aaafef22517a1ff944cf4c030
erik.masny <erik.masny@gmail.com>
2021-04-04 18:31:45 +0200

    Added chart.js code and modal

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -1,1 +1,91 @@
-console.log('Loaded')
+import Chart from 'chart.js/auto';
+import $ from 'jquery';
+import 'bootstrap';
+
+$(document).ready(function() {
+    $('.lectureStudentDetail').on('submit', function(e) {
+        e.preventDefault();
+
+        var modal = $('#exampleModal');
+        var modalContent = modal.find( '.modal-body' );
+
+        var student_id = $(this).find('input[name=student_id]').val();
+        var lecture_id = $(this).find('input[name=lecture_id]').val();
+        var url = 'http://localhost/webtech/zadanie4' + `/student/${student_id}/lecture/${lecture_id}`;
+        $.ajax({
+            type: "GET",
+            url: url,
+            success: function(data) {
+                data = JSON.parse(data);
+                if( data ) {
+                    var output = '<table class="table">';
+                    data.forEach(element => {
+                        output += '<tr>';
+                        output += '<td>' + element.status + '</td>';
+                        output += '<td>' + element.time + '</td>';
+                        output += '</tr>';
+                    });
+    
+                    output += '</table>';
+                } else {
+                    var output = "Nothing to be found here";
+                }
+                modalContent.html(output);
+                modal.modal()
+            },
+            fail: function(err) {
+                console.log(err)
+            }
+
+        })
+    })
+
+
+    if(lectures) {
+        var ctx = document.getElementById('myChart');
+
+        var labels = [];
+        var data = [];
+
+        lectures.forEach( (lecture,index) => {
+            labels.push( ++index )
+            data.push(lecture.numOfStudents)
+
+        })
+
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: 'Účasť',
+                    data: data,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    }
+})
